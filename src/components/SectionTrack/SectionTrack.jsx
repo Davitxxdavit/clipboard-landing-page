68ff2d3f27224daa3754fac8c301de5d808b1e36
zaden <davitberidze1510@gmail.com>
2024-08-14 17:12:47 +0400

    third commit

--- src/components/SectionTrack/SectionTrack.jsx
+++ src/components/SectionTrack/SectionTrack.jsx
@@ -10,7 +10,7 @@ const SectionTrack = () => {
             <p> Clipboard instantly stores any item you copy in the cloud, meaning you can access your snippets immediately on all your devices. Our Mac and iOS apps will help you organize everything.</p>
         </div>
         <div className='track-second'>
-            <img src={computerImage} />
+            <img src={computerImage} alt='#' />
             <div className='content'>
                 <div className='content-inner'>
                     <h3>Quick Search</h3>
